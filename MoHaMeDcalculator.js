7bbd642c81bf7fd1dd37823a71c69ac66b499901
m0hamed-mustafa <120212092+m0hamed-mustafa@users.noreply.github.com>
2022-12-09 20:30:33 +0200

    Add files via upload

---
+++ MoHaMeDcalculator.js
@@ -0,0 +1,12 @@
+function display(id) {
+    document.getElementById("actions").innerHTML+=document.getElementById(id).innerHTML ;
+}
+
+function clean () {
+    document.getElementById("actions").innerHTML="";
+    document.getElementById("screen").innerHTML="0";
+}
+
+function calc () {
+    document.getElementById("screen").innerHTML=eval(document.getElementById("actions").innerHTML);                
+}
